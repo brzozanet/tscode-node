9b12ee1f33a823b756c30109ec4cb6fb8703dc37
Paweł Brzozowski <pawel@brzoza.net>
2024-03-17 14:58:09 +0100

    Drink automat

--- module-02/automat.js
+++ module-02/automat.js
@@ -1,2 +1,17 @@
-const drinkGenerator = process.argv[2];
-console.log(drinkGenerator);
+const drinkType = process.argv[2];
+const drinkSize = process.argv[3].slice(5);
+
+let drinkTypePl;
+
+switch (drinkType) {
+  case "name=juice":
+    drinkTypePl = "sok";
+    break;
+  case "name=coffe":
+    drinkTypePl = "kawę";
+    break;
+  default:
+    console.log("Wybrano nieprawidłową opcję");
+}
+
+console.log(`Przygotowuję ${drinkTypePl} o pojemności ${drinkSize} ml.`);
